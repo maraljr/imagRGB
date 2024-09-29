ac94577518c4cdb5ca2171d2847e29bb5eb4b78f
maraljr <maralj@iastate.edu>
2024-09-29 18:48:47 -0500

    Updated main to create RGB colors from images

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # imagRGB
 Project for converting images to color palettes. Fall-2024-HCI-584
+
+## Dependencies and Running the Project
+To run this project you will need the following installed:
+- Pillow
+- numpy
+- scikit-learn
+
+A [requirements.txt](./requirements.txt) file is included in the project, so can install these libraries by running `pip install -r requirements.txt` from the root of the project.
+
+Run the application by running main.py, which includes hard-coded values for `IMAGE_PATH` and `NUM_COLORS` that you can change to get different results.
+
+## Attribution
+- [Example images](./example-images/) in this project were sourced from [https://unsplash.com/](https://unsplash.com/)
--- main.py
+++ main.py
@@ -1,3 +1,30 @@
+from PIL import Image
+from sklearn.cluster import KMeans
+import numpy as np
+
+def extract_colors(image_path, num_colors):
+    image = Image.open(image_path)
+    image = image.resize((150, 150))
+    image_array = np.array(image)
+    pixels = image_array.reshape(-1, 3)
+    
+    kmeans = KMeans(n_clusters=num_colors)
+    kmeans.fit(pixels)
+    
+    colors = kmeans.cluster_centers_
+    colors = colors.round().astype(int)
+    
+    return colors
+
+def main():
+    IMAGE_PATH = "example-images/fog-mountain-green-landscape.jpg"
+    NUM_COLORS = 4
+
+    colors = extract_colors(IMAGE_PATH, NUM_COLORS)
+    
+    print(f"Dominant colors in {IMAGE_PATH}:")
+    for color in colors:
+        print(f"RGB: {tuple(color)}")
 
 if __name__ == "__main__":
-    print("hello world")
+    main()
