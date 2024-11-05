fb1b9a94e95ce52306c5c8f44c8a080b1b8527e2
maraljr <maralj@iastate.edu>
2024-11-04 18:23:14 -0600

    Version 2 progress report A

--- README.md
+++ README.md
@@ -6,10 +6,28 @@ To run this project you will need the following installed:
 - Pillow
 - numpy
 - scikit-learn
+- opencv-python
 
 A [requirements.txt](./requirements.txt) file is included in the project, so you can install these libraries by running `pip install -r requirements.txt` from the root of the project.
 
-Run the application by running main.py, which includes hard-coded values for `IMAGE_PATH` and `NUM_COLORS` that you can change to get different results.
+Run the application by running [main.py](./main.py): `python main.py [IMAGE_PATH]`. You must provide the image path as a positional argument. You can provide other optional arguments to adjust the output. Run `python main.py --help` for a list of arguments.
+```
+Generate a color palette from an image.
+
+positional arguments:
+  image_path            Path to the input image file.
+
+options:
+  -h, --help            show this help message and exit
+  --num_colors NUM_COLORS
+                        Number of colors to extract from the image. 1-10. Default is 5.
+  --output_dir OUTPUT_DIR
+                        Directory to save the color palette image. Default is ./palettes/
+  --output_name OUTPUT_NAME
+                        Name of the output color palette image. Default is the input file name plus added data.
+  --percentile PERCENTILE
+                        Percentile for color extraction. 1-100. Default is 15.
+```
 
 ## Attribution
 - [Example images](./example-images/) in this project were sourced from [https://unsplash.com/](https://unsplash.com/)
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from PIL import Image, ImageDraw, ImageFont
+import cv2
 from sklearn.cluster import KMeans
 import numpy as np
 import os
+import argparse
 
+# TODO: delete this function if not used in final project
 def extract_colors(image_path, num_colors):
     image = Image.open(image_path)
     image = image.resize((150, 150))
@@ -16,6 +19,21 @@ def extract_colors(image_path, num_colors):
     colors = colors.round().astype(int)
     
     return colors
+
+def extract_palette(image_path, n_colors, percentile):
+    # Load and preprocess the image
+    image = cv2.imread(image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = image.reshape((image.shape[0] * image.shape[1], 3))
+    # Apply K-Means to get initial clusters
+    kmeans = KMeans(n_clusters=n_colors, random_state=0).fit(image)
+    labels = kmeans.labels_
+    # Calculate the percentile-based centroids
+    centroids = np.zeros((n_colors, 3))
+    for i in range(n_colors):
+        cluster_points = image[labels == i]
+        centroids[i] = np.percentile(cluster_points, percentile, axis=0)
+    return centroids.round().astype(int)
 
 def create_color_palette(colors, output_path):
     color_height = 200
@@ -49,17 +67,35 @@ def create_color_palette(colors, output_path):
     palette.save(output_path, dpi=(300, 300))
     print(f"Color palette saved to {output_path}")
 
+def check_range(value, min_val, max_val):
+    ivalue = int(value)
+    if ivalue < min_val or ivalue > max_val:
+        raise argparse.ArgumentTypeError(f"Value must be between {min_val} and {max_val}")
+    return ivalue
+
 def main():
-    IMAGE_PATH = "example-images/fog-mountain-green-landscape.jpg"
-    NUM_COLORS = 4
-    OUTPUT_DIR = "./palletes/"
+    parser = argparse.ArgumentParser(description="Generate a color palette from an image.")
+    parser.add_argument("image_path", type=str, help="Path to the input image file.")
+    parser.add_argument("--num_colors", type=lambda x: check_range(x, 1, 10), default=5, help="Number of colors to extract from the image. 1-10. Default is 5.")
+    parser.add_argument("--output_dir", type=str, default="./palettes/", help="Directory to save the color palette image. Default is ./palettes/")
+    parser.add_argument("--output_name", type=str, default="_", help="Name of the output color palette image. Default is the input file name plus added data.")
+    parser.add_argument("--percentile", type=lambda x: check_range(x, 1, 100), default=15, help="Percentile for color extraction. 1-100. Default is 15.")
 
-    file_name = IMAGE_PATH.split("/")[-1].split(".")[0]
-    colors = extract_colors(IMAGE_PATH, NUM_COLORS)
+    args = parser.parse_args()
+
+    file_name = args.image_path.split("/")[-1].split(".")[0]
+    colors = extract_palette(args.image_path, args.num_colors, args.percentile)
     
-    os.makedirs(os.path.dirname(OUTPUT_DIR), exist_ok=True)
+    os.makedirs(os.path.dirname(args.output_dir), exist_ok=True)
 
-    create_color_palette(colors, f"{OUTPUT_DIR}{file_name}-palette.png")
+    output_path = ""
+    # TODO: add better handling for user input (add .png if not present, including trailing / for output_dir if not present)
+    if args.output_name == "_":
+        output_path = f"{args.output_dir}{file_name}-palette-colors-{args.num_colors}-percentile-{args.percentile}.png"
+    else:
+        output_path = f"{args.output_dir}{args.output_name}.png"
+
+    create_color_palette(colors, output_path)
 
 if __name__ == "__main__":
     main()
